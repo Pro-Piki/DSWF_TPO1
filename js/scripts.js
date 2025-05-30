405423137df5b5aa3a55aa6a66a90d869fc28f08
barviz <barbaravizgarra@gmail.com>
2025-05-30 12:20:08 -0300

    fix mr

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 async function includeHTML(id, url) {
   const response = await fetch(url);
   const html = await response.text();
   document.getElementById(id).innerHTML = html;
 }
-=======
-// opcional porque aún no lo vimos
->>>>>>> b957d74 (restructuracion de carpetas)
